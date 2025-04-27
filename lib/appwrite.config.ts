99b9dfb31527066b3052d1799213316d79f35733
tarak6984 <tareksabbir20@gmail.com>
2025-04-28 00:12:34 +0600

    Fixed api issues

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,18 +1,20 @@
 import * as sdk from "node-appwrite";
 
-export const {
-  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+const {
+  NEXT_PUBLIC_ENDPOINT,
   PROJECT_ID,
   API_KEY,
   DATABASE_ID,
   PATIENT_COLLECTION_ID,
   DOCTOR_COLLECTION_ID,
   APPOINTMENT_COLLECTION_ID,
-  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+  NEXT_PUBLIC_BUCKET_ID,
 } = process.env;
 
-const client = new sdk.Client();
+export const ENDPOINT = NEXT_PUBLIC_ENDPOINT;
+export { PROJECT_ID, API_KEY, DATABASE_ID, PATIENT_COLLECTION_ID, DOCTOR_COLLECTION_ID, APPOINTMENT_COLLECTION_ID, NEXT_PUBLIC_BUCKET_ID as BUCKET_ID };
 
+const client = new sdk.Client();
 client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
